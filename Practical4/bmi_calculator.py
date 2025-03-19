5a40026004c17ecfc718f4f38cbab1bde1e02945
dudiuyuan <1297965820@qq.com>
2025-03-19 14:04:47 +0800

    Update bmi_calculator.py

--- Practical4/bmi_calculator.py
+++ Practical4/bmi_calculator.py
@@ -1,14 +1,14 @@
-#iuput weight and height
-#turn into float and calculate BMI
-#output BMI
-#output if underweight, normal weight or obese
+#1.iuput weight and height
+#2.turn into float and calculate BMI
+#3.output BMI
+#4.output if underweight, normal weight or obese
 
-a=input("weight in kg:")
+a=input("weight in kg:")   #1.iuput weight and height
 b=input("height in m:")
-BMI=float(a)/(float(b)**2)
+BMI=float(a)/(float(b)**2)   #2.turn into float and calculate BMI
 
-print("Your BMI is: ",BMI)
-if BMI<18.5:
+print("Your BMI is: ",BMI)    #3.output BMI
+if BMI<18.5:                                               #4.output if underweight, normal weight or obese
     print("I'm sorry, but You are Underweight")
 elif 18.5<=BMI<30:  
     print("Great! Yoe are Normal weight")
